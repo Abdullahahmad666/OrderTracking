5c03b5c1baa3dfd6663c555d0386c464ced3a0fe
Abdullahahmad666 <l201326@lhr.nu.edu.pk>
2024-10-31 22:25:34 +0500

    Initial commit

--- app/src/main/java/com/example/navigation_smd_7a/MainActivity.java
+++ app/src/main/java/com/example/navigation_smd_7a/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-
+                dialog.show();
             }
         });
 
--- app/src/main/java/com/example/navigation_smd_7a/NewOrderFragment.java
+++ app/src/main/java/com/example/navigation_smd_7a/NewOrderFragment.java
@@ -2,72 +2,30 @@ package com.example.navigation_smd_7a;
 
 import android.content.Context;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NewOrderFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NewOrderFragment extends Fragment {
 
-    Context context;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Context context;
+    private ListView lvNewOrderList;
+    private ProductAdapter adapter;
+    private ArrayList<Product> products = new ArrayList<>();
+    private ProductDB productDB;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NewOrderFragment() {
-        // Required empty public constructor
-    }
+    // Required empty public constructor
+    public NewOrderFragment() {}
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         this.context = context;
-    }
-
-
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewOrderFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewOrderFragment newInstance(String param1, String param2) {
-        NewOrderFragment fragment = new NewOrderFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -80,13 +38,29 @@ public class NewOrderFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ListView lvNewOrderList = view.findViewById(R.id.lvNewOrdersList);
-        ProductDB productDB = new ProductDB(context);
+
+        // Initialize ListView and ProductDB
+        lvNewOrderList = view.findViewById(R.id.lvNewOrdersList);
+        productDB = new ProductDB(context);
+
+        // Set up the adapter with the initial empty product list
+        adapter = new ProductAdapter(context, R.layout.product_item_design, products);
+        lvNewOrderList.setAdapter(adapter);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshProductList(); // Refresh the product list every time the fragment becomes visible
+    }
+
+    // Method to load products from the database and refresh the ListView
+    private void refreshProductList() {
         productDB.open();
-        ArrayList<Product> products = productDB.fetchProducts();
+        products.clear();
+        products.addAll(productDB.fetchProducts()); // Fetch updated product list
         productDB.close();
 
-        ProductAdapter adapter = new ProductAdapter(context, R.layout.product_item_design, products);
-        lvNewOrderList.setAdapter(adapter);
+        adapter.notifyDataSetChanged(); // Notify adapter of data changes to update the ListView
     }
 }
